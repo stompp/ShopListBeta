c1d2314469976023dd0440e2f9fe7fbfed681ffd
J.M. Tomé <josemiguel.tomejimenez@gmail.com>
2016-09-22 00:06:05 +0200

    Some files ignored Restarted WPJSONAPI Classes definitions

--- app/src/main/java/com/riggitt/utils/wpjson/api/WPJSONApi.java
+++ app/src/main/java/com/riggitt/utils/wpjson/api/WPJSONApi.java
@@ -8,36 +8,4 @@ public class WPJSONApi {
     public static final String SERVER_URL = "wplispra.riggitt.org";
     public static final String API_BASE = "api";
 
-    public static String getControllerURL(String controller){
-        StringBuilder sb = new StringBuilder();
-        sb.append(SERVER_URL);
-        sb.append("/");
-        sb.append(API_BASE);
-        sb.append("/");
-        sb.append(controller);
-        sb.append("/");
-        return sb.toString();
-    }
-
-    public static String getControllerMethodURL(String controller,String method){
-        StringBuilder sb = new StringBuilder();
-        sb.append(WPJSONApi.getControllerURL(controller));
-        sb.append(method);
-        sb.append("/");
-
-        return sb.toString();
-    }
-
-
-    public interface RequestDoneListener{
-        public void onRequestDone(Response response);
-    }
-
-    public static boolean startJSONAPIUSERAuthentication(String user,String password,RequestDoneListener requestDoneListener){
-
-        return true;
-
-    }
-
-
 }
